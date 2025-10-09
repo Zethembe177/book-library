0f422a7002e56e7a7df3936224b581864691f4aa
Zethembe177 <139282326+Zethembe177@users.noreply.github.com>
2025-10-09 11:10:44 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,13 @@ console.log("🔄 Starting backend server...");
 
 // Database connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "zamabuhle",  // your MySQL password
-  database: "Book_Library"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
+
 
 db.connect(err => {
   if (err) throw err;
