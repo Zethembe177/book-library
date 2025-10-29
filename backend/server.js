ad471ddac13959a75bbcf1aca6b12ee47eb68006
Zethembe177 <139282326+Zethembe177@users.noreply.github.com>
2025-10-29 12:15:44 +0200

    Update CORS origin for production deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://book-library-production-eb37.up.railway.app"],
+    origin: ["http://localhost:5173", "https://book-library-zet.netlify.app",],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
