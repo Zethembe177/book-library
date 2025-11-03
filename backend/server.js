0db54c0d803d2d31ab218c190800fce2b9039278
Zethembe177 <139282326+Zethembe177@users.noreply.github.com>
2025-11-03 10:09:32 +0200

    Change database connection to ES module syntax

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import createBooksRoutes from "./routes/books.js";
-const connectDb = require("./db.js"); // if you use module.exports
+import connectDb from "./db.js"; // ✅ correct for ES modules
+
 
 
 dotenv.config();
